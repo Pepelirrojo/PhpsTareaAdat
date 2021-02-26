af54b7a572f849113e3c515747dc0f5eb3afd1b0
pepelirrojo <pepelirrojo>
2021-02-26 21:56:58 +0100

    Borrar vuelo!

--- borrarVuelo.php
+++ borrarVuelo.php
@@ -1,83 +1,35 @@
+
 <?php
 
-function borrarVuelo($conn){
-	$arrEsperado = array(
-    "peticion" => "delete",
-    "vuelos" => array(
-    	"id" => "10"
-    )
-    );
+require 'vendor/autoload.php';
 
-function JSONCorrectoAnnadir($recibido){
-    $aux = false;
-    if(isset($recibido["peticion"]) && $recibido["peticion"]=="delete" ){
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
-        if(isset($recibido["vuelos"])){
+$cliente = new MongoDB\Client("mongodb://localhost:27017");
 
-            $auxVuelo = $recibido["vuelos"];
-
-            if(isset($auxVuelo["id"])){
-                    $aux = true;
-                }
-        }
-
-    }
-
-    return $aux;
-}
-
-$arrMensaje = array();
+$coleccion = $cliente->adat_vuelos->vuelos_compra;
 
 $parameters = file_get_contents("php://input");
 
-if(isset($parameters)){
+$arrayInfo = array();
 
-    $mensajeRecibido = json_decode($parameters, true);
+$datosViajero = array();
 
-	if(JSONCorrectoAnnadir($mensajeRecibido)){
+if (isset($parameters)) {
+  $jsonRecibido = json_decode($parameters, true);
 
-		$vuelo = $mensajeRecibido["vuelos"];
+    $codigo = $jsonRecibido["codigo"];
+	$resultado = $coleccion->deleteOne(['codigo' => $codigo]);
 
-		$id = $vuelo["id"];
-
-		$query  = "DELETE FROM vuelos WHERE ID = $id";
-
-		$result = $conn->query ( $query );
-
-		if (isset ( $result ) && $result) {
-
-			$arrMensaje["estado"] = "ok";
-			$arrMensaje["mensaje"] = "Vuelo BORRADO correctamente";
-			$lastId = $conn->insert_id;
-			$arrMensaje["lastId"] = $lastId;
-
-		}else{
-
-			$arrMensaje["estado"] = "error";
-			$arrMensaje["mensaje"] = "SE HA PRODUCIDO UN ERROR AL ACCEDER A LA BASE DE DATOS";
-			$arrMensaje["error"] = $conn->error;
-			$arrMensaje["query"] = $query;
-
-		}
-
-
-	}else{
-
-		$arrMensaje["estado"] = "error";
-		$arrMensaje["mensaje"] = "EL JSON NO CONTIENE LOS CAMPOS ESPERADOS";
-		$arrMensaje["recibido"] = $mensajeRecibido;
-		$arrMensaje["esperado"] = $arrEsperado;
-	}
-
-}else{
-
-	$arrMensaje["estado"] = "error";
-	$arrMensaje["mensaje"] = "EL JSON NO SE HA ENVIADO CORRECTAMENTE";
-
-}
-$mensajeJSON = json_encode($arrMensaje,JSON_PRETTY_PRINT);
+$arrayInfo["estado"] = $resultado;
+$arrayInfo["mensaje"] = "Borrado correctamente";
+$mensajeJSON = json_encode($arrayInfo, JSON_PRETTY_PRINT);
 echo $mensajeJSON;
-
+}else {
+  $arrayInfo["estado"] = false;
+  $arrayInfo["mensaje"] = "Fallo al insertar el vuelo";
 }
 
 ?>
